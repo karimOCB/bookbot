bd62618d72732de30b9d220f79b5abe716243452
Karim <chaya.karim.omar@gmail.com>
2025-10-25 15:38:14 +0000

    responsive to console argument input

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # bookbot
 
 BookBot is my first [Boot.dev](https://www.boot.dev) project!
+
+But not my first project at all!
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import os
+import sys
 from stats import get_words_text, get_characters_text, get_sorted_chars
 
 def get_book_text(path_to_file):
@@ -7,14 +7,16 @@ def get_book_text(path_to_file):
         return file_contents
 
 def main():
-    book_path = os.path.join("books", "frankenstein.txt")
-    content = get_book_text(book_path)
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    content = get_book_text(sys.argv[1])
     num_words = get_words_text(content)
     num_chars = get_characters_text(content)
     ordered_chars = get_sorted_chars(num_chars)
     print(f"""
     ============ BOOKBOT ============
-    Analyzing book found at {book_path}
+    Analyzing book found at {sys.argv[1]}
     ----------- Word Count ----------
     Found {num_words} total words
     --------- Character Count ------- """)
